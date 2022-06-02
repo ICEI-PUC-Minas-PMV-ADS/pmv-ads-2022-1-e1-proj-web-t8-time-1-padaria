8761ab7b9a31e80cf6f6ab1ff5b19ab0e3e8e135
LeoXavier13 <103225086+LeoXavier13@users.noreply.github.com>
2022-06-02 18:30:38 -0300

    Add files via upload

---
+++ src/Quem somos/faleconosco.js
@@ -0,0 +1,26 @@
+var contato = [
+    {
+        "nome": "SAC -      ",
+        "Contato": "1234.1234 - ",
+        "email": "sac@fornada.com.br"
+    },
+    {
+        "nome": "Administrativo - ",
+        "Contato": "9878.2345 - ",
+        "email": "adm@fornada.com.br"
+    },
+    {
+        "nome": "Financeiro - ",
+        "Contato": "1234.1234 - ",
+        "email": "financeiro@fornada.com.br"
+    }
+]
+
+function exibirContatos(){
+    var textoHTML = '';
+    for(let x=0; x<contato.length; x++){
+        textoHTML += `${contato[x].nome + contato[x].Contato + contato[x].email} <br>`;
+    };
+    var tela = document.getElementById('fale');
+    tela.innerHTML = textoHTML;
+}
